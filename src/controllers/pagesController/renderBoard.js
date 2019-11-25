a1ab59a1c50884d055b0f947612534759fe49feb
Joao Zsigmond <jzsiggy@gmail.com>
2019-11-25 18:28:32 -0200

    Fixing styling

--- src/controllers/pagesController/renderBoard.js
+++ src/controllers/pagesController/renderBoard.js
@@ -3,6 +3,15 @@ const { Post } = require('../../models');
 const renderBoard = (request, response, next) => {
   Post.find({ })
   .then(postList => {
+    formattedPostList = postList.forEach(post => {
+      postDate = post.createdAt;
+      post.formattedDate = postDate.getDate() + "-" +
+                          (postDate.getMonth() + 1) + "-" + 
+                          postDate.getFullYear()+ " " + 
+                          postDate.getHours() + ":" + 
+                          postDate.getMinutes() + ":" + 
+                          postDate.getSeconds() ;
+    });
     response.render("dashboard", {
       posts : postList,
     });
