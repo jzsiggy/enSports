aa33073a9b4794dce2e4f59e63c39a625f56f48e
Joao Zsigmond <jzsiggy@gmail.com>
2019-11-27 18:37:30 -0200

    Responsiveness to btn change color o scroll

--- src/public/javascripts/home.js
+++ src/public/javascripts/home.js
@@ -1,23 +1,14 @@
 window.addEventListener('scroll', () => {
   let buttons = document.querySelectorAll('.menu > div > a');
 
-  let hambMenu = document.querySelectorAll('.hamburger-inner, .hamburger-inner::before, .hamburger-inner::after');
-
-  console.log(hambMenu);
   let scrollHeight = window.pageYOffset;
-  if ( scrollHeight > screen.height ) {
+  if ( scrollHeight > window.innerHeight * 0.90 ) {
     for (btn of buttons) {
       btn.style.color = "black";
-    }
-    for (element of hambMenu) {
-      element.style.backgroundColor = "black";
     }
   } else {
     for (btn of buttons) {
       btn.style.color = "white";
-    }
-    for (element of hambMenu) {
-      element.style.backgroundColor = "white";
     }
   }
 });
